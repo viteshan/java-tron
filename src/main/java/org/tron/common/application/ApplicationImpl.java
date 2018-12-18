1e47ee0e68bcc6a1036c2f2cfb6f2f666b0d13ff
wubin1 <wubin1@tron.network>
2018-12-18 10:53:46 +0800

    feature check db status only for test

--- src/main/java/org/tron/common/application/ApplicationImpl.java
+++ src/main/java/org/tron/common/application/ApplicationImpl.java
@@ -1,8 +1,13 @@
 package org.tron.common.application;
 
+import com.google.protobuf.ByteString;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.tron.common.utils.ByteArray;
+import org.tron.core.capsule.BlockCapsule;
 import org.tron.core.config.args.Args;
 import org.tron.core.db.BlockStore;
 import org.tron.core.db.Manager;
@@ -10,6 +15,9 @@ import org.tron.core.net.node.Node;
 import org.tron.core.net.node.NodeDelegate;
 import org.tron.core.net.node.NodeDelegateImpl;
 import org.tron.core.net.node.NodeImpl;
+import org.tron.protos.Protocol.Block;
+import org.tron.protos.Protocol.BlockHeader;
+import org.tron.protos.Protocol.BlockHeader.raw;
 
 @Slf4j
 @Component
@@ -66,8 +74,32 @@ public class ApplicationImpl implements Application {
 
   @Override
   public void shutdown() {
-    
+    ExecutorService executorService = Executors.newFixedThreadPool(2);
+    BlockCapsule blockCapsule = new BlockCapsule(Block.newBuilder().setBlockHeader(
+      BlockHeader.newBuilder().setRawData(raw.newBuilder().setParentHash(ByteString.copyFrom(
+        ByteArray
+          .fromHexString("0304f784e4e7bae517bcab94c3e0c9214fb4ac7ff9d7d5a937d1f40031f87b81")))
+      )).build());
+    for (int i =0; i < 1000000; i ++) {
+      executorService.execute(new Runnable() {
+        @Override
+        public void run() {
+          blockStoreDb.put(blockCapsule.getData(), blockCapsule);
+        }
+      });
+      executorService.execute(new Runnable() {
+        @Override
+        public void run() {
+          blockStoreDb.getBlockByLatestNum(4);
+        }
+      });
+    }
 
+    try {
+      executorService.wait(1000000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
 
     logger.info("******** begin to shutdown ********");
     synchronized (dbManager.getRevokingStore()) {
